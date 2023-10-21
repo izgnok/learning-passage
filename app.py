36271ec6213ae5e5d8356d9267d4e429b40b9441
jhparkland <pwjdgus1017@donga.ac.kr>
2023-10-21 20:54:11 +0900

    test

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ external_stylesheets = ['assets/css/style.css']  # CSS 파일명
 fig = px.Figure(data=[px.Scatter(x=[1, 2, 3], y=[4, 1, 2])])
 
 app = Dash('carbon-friendly', external_stylesheets=external_stylesheets)
+server = app.server
 app.title = 'Carbon friendly'
 app._favicon = 'assets/favicon/favicon.ico'
 
